0ec6e66f3dfe50d1458ef59dd0d0045a0f27382e
Nguyen Hai Dang <mr.pi.cbg@gmail.com>
2020-06-25 08:32:59 +0700

    Add + Delete Product and Category

---
+++ DA_KTLT_1988010/Pages/DeleteCategory.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DA_KTLT_1988010.BLL.Repositoties;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DA_KTLT_1988010.Pages
+{
+    public class DeleteCategoryModel : PageModel
+    {
+        public void OnGet(int id)
+        {
+            var categoryRepo = new CategoryRepo();
+            bool success = categoryRepo.Delete(id);
+            if (success)
+                ViewData["DeleteCategory"] = "Delete category successfully.";
+            else ViewData["DeleteCategory"] = $"Error! There's no product with ID = {id}.";
+        }
+    }
+}
